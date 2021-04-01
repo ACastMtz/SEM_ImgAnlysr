d9b495c91e5ae7747e30816b090deef821dc1a00
ACastMtz <aron.castro.m@gmail.com>
2021-04-01 17:24:41 +0200

    Modified bzw. added methods

--- src/NW_counter_final.py
+++ src/NW_counter_final.py
@@ -11,15 +11,18 @@ from sklearn.metrics import silhouette_samples, silhouette_score
 
 class NW_SEM_image:
     '''Defining metadata (instance attributes)'''
-    def __init__(self, path, size, tilt, magn, pitch, metric_size):
+    def __init__(self, path, size, tilt, magn, pitch, metric_size, verbosity):
         self.path = path 
         self.size = size
         self.tilt = tilt 
         self.magn = magn
         self.pitch = pitch
         self.metric_size = metric_size
-        self.__imgWidth = self.size[1]
-        self.__imgHeight = self.size[0]
+        self.verbosity = verbosity
+        self.__imgWidth = size[0]
+        self.__imgHeight = size[1]
+        self.__nmToPx = 0
+        
         return  
     
     '''Print instance's info'''
@@ -30,11 +33,16 @@ class NW_SEM_image:
             Tilt: {self.tilt} degrees \n \
             Magnification: {self.magn}k \n \
             Pitch: {self.pitch} \n \
-            Metric: {self.metric_size} \n\
+            Metric: {self.metric_size} \n \
             Width: {self.__imgWidth} \n \
-            Height: {self.__imgHeight} \n\
+            Height: {self.__imgHeight} \n \
+            Verbosity: {self.verbosity} \n \
             "
         return img_info
+
+    '''Load image'''
+    def img_loader(self):
+        return cv2.imread(self.path)
 
     '''Check image parameters'''
     def params_ctrl(self):
@@ -47,6 +55,7 @@ class NW_SEM_image:
             key = 2
         else:
             raise ValueError('Not supported set of image parameters')
+        
         return key
 
     '''Switcher'''
@@ -63,7 +72,8 @@ class NW_SEM_image:
         func = switcher.get(key, lambda: 'Not supported set of image parameters')
         return func()
 
-    
+    # ----------------------------------------------------------------------------------- #
+
     def img_processing(self):
         '''
         Method to crop, find length scale, etc. of the image.
@@ -72,9 +82,9 @@ class NW_SEM_image:
         of the reference metric (to draw a rectangle around it). 
         NOTE: The leftmost horizontal vertex is defined in the Switcher function.
         '''
-        img = cv2.imread(self.path)
+        img = self.img_loader()
         img_h, img_w = img.shape[0:2]
-        if [img_h, img_w] != self.size:
+        if [img_w, img_h] != self.size:
             raise ValueError('Please enter an accepted image size in pixels')
         key = self.params_ctrl()
 
@@ -84,33 +94,40 @@ class NW_SEM_image:
         y1_ls = int(0.94*img_h)
         y2_ls = int(0.96*img_h)
         ls_width = x2_ls-x1_ls
-        nm_per_px = self.metric_size/ls_width
+        self.nmToPx = self.metric_size/ls_width
         rect_thickness = 5
         cv2.rectangle(img, (x1_ls,y1_ls), (x2_ls,y2_ls), (255, 0, 0), rect_thickness)
 
-        # Crop image's height by 10%
+        # Crop image
         crop_h = img_h - int(img_h/10)
         img_cr = img[0:crop_h,:]
 
         return img, img_cr
-       
-        
-# num_clusters = 6
-# top_clusters = [1,2]
-
-    '''Method to find adequate threshold for image filtering'''
-    def img_threshold(self):
-        
-        return
 
     '''Method to filter the image'''
-    def img_filtering(self):
+    def img_filtering(self, n_clusters):
+
+        img, img_cr = self.img_processing()
+
+        # KMeans thresholding (Pixel clustering)
+        grayscale = cv2.cvtColor(img_cr , cv2.COLOR_BGR2GRAY)
+        grayscale_n = grayscale.reshape(grayscale.shape[0]*grayscale.shape[1],1)/255
+        clusterer = KMeans(n_clusters=n_clusters, random_state=0).fit(grayscale_n)
+        threshold = np.sort(clusterer.cluster_centers_, axis=0)
+        if self.verbosity == True:
+            print('Threshold Center Values: \n {}'.format(threshold))
         
-        return
+        # Binarize image
+        thr = int(threshold[n_clusters-1][0]*255)
+        ret, bw = cv2.threshold(grayscale, thr, 255, cv2.THRESH_BINARY)
+
+        return grayscale_n, bw
 
     '''Method to print images'''
     def printer(self, num_plots):
         img, img_cr = self.img_processing()
+        grayscale_n , bw = self.img_filtering(n_clusters=3)
+
         fig = plt.figure()
         ax1 = fig.add_subplot(1,num_plots,1)
         ax1.imshow(img)
@@ -120,22 +137,29 @@ class NW_SEM_image:
         ax2.imshow(img_cr)
         plt.title('Cropped Image')
         plt.axis('off')
+        ax3 = fig.add_subplot(1,num_plots,3)
+        counts , vals = np.histogram(grayscale_n, bins=500)
+        plt.plot(np.linspace(0,1, num=500), counts, label='original')
+        plt.title('Grayscale image histogram')
+        plt.xlabel('Pixel intensity')
+        plt.ylabel('Count')
         plt.show()
         return
 
 
 img_path = 'Images\InGaAs\M3_2390_50nm0.4um-15Tilt-10K.jpg'
-# size = [1280,960]
-size = [960,1280]
+size = [1280,960]
+# size = [960,1280]
 tilt = 15
 magn = 25
 pitch = 400
 metric_size = 5000
+num_plots = 3
+verbosity = True
 
 # Instance
-test_img = NW_SEM_image(path=img_path, size=size, tilt=tilt, magn=magn, pitch=pitch, metric_size=metric_size)
+test_img = NW_SEM_image(path=img_path, size=size, tilt=tilt, magn=magn, pitch=pitch, metric_size=metric_size, verbosity=verbosity)
 print(test_img)
 
 # Methods
-# test_img.img_processing()
-test_img.printer(num_plots=2)
+test_img.printer(num_plots=num_plots)
